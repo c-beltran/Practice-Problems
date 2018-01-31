55f77acf313bec0a65da3e3e9b2deed2e5451bf6
c-beltran <carlosbeltran90@gmail.com>
2018-01-31 12:46:05 -0500

    set up environment, started first problem

---
+++ AlgorithmPractice/src/InsertionSort.java
@@ -0,0 +1,12 @@
+//insertion sort is useful and efficient
+//when sorting a small number of elements
+public class InsertionSort {
+	public static void main(String[] args){
+		//given the following array
+		int A[] = {5,2,4,6,1,3};
+		int size = A.length;
+		for(int j=1; j<size; j++){
+			int key = A[j];
+		}
+	}
+}
