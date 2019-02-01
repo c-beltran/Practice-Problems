2ca7a303c349b433ac054140039a54eda3258fde
Carlos A Beltran <carlosbeltran90@gmail.com>
2019-02-01 08:56:36 -0500

    added engine functionality

--- PracticeCar/src/Car.java
+++ PracticeCar/src/Car.java
@@ -1,8 +1,13 @@
 
 public class Car {
 	
+	Engine TYT2JZ = new Engine();
+	
 	public void run(){
-		System.out.println("car is running");
+		System.out.println("Turning car on...");
+		TYT2JZ.carPartInMotion();
+		TYT2JZ.function();
+		TYT2JZ.status();		
 	}
 
 }
--- PracticeCar/src/Engine.java
+++ PracticeCar/src/Engine.java
@@ -1,4 +1,31 @@
+public class Engine extends CarPart {
 
-public class Engine {
+	int cylinderCount = 4;
+	int tempInCelsius = 90;
+	String oilType = "0W30";
+	boolean engineStart = false;
+	
+	
+	@Override
+	public void function() {
+		System.out.println("Engine function is to provide the suficient power to make car do work.");
+		
+	}
+
+	@Override
+	public void status() {
+		if(engineStart == true){
+			int wearAndTear = 25;
+			engineStatus = engineStatus - wearAndTear;
+		}
+		System.out.println("Engine has about " + engineStatus + "%" + " of life remaining.");
+		
+	}
+
+	@Override
+	public void carPartInMotion() {
+		engineStart = true;
+		System.out.println("Engine is running...");
+	}
 
 }
